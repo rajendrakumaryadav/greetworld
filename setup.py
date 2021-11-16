013d3a19b2638e79e929a7419ffb68e050d367d5
Rajendra Kumar R Yadav <13816347+rajendrakumaryadav@users.noreply.github.com>
2021-11-17 01:16:29 +0530

    Refactoring

--- setup.py
+++ setup.py
@@ -14,14 +14,14 @@ setuptools.setup(
     description="Hello World message",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/rajendrakumaryadav/hello_world",
+    url="https://github.com/rajendrakumaryadav/greetworld",
     # packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "hello_world"},
-    packages=setuptools.find_packages(where="hello_world"),
+    package_dir={"": "greetworld"},
+    packages=setuptools.find_packages(where="greetworld"),
     python_requires=">=3.5",
 )
