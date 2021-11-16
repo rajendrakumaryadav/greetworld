9847cf4fc99dd90b4c87342ca9ac77790ec2d9cc
Rajendra Kumar R Yadav <13816347+rajendrakumaryadav@users.noreply.github.com>
2021-11-17 00:43:54 +0530

    minor update

--- hello_world/__init__.py
+++ hello_world/__init__.py
@@ -5,7 +5,7 @@ from hello_world import greet
 and call the after creating object. 
 Call the methods in class
 '''
-__name__ = "hello_world"
+__name__ = "greeting"
 __version__ = 0.1
 __author__ = 'Rajendra Kumar R Yadav'
 __doc__ = '''
