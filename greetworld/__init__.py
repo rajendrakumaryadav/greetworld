81cdfbc4cadbee1563ffde00260157f72e2914bf
Rajendra Kumar Yadav <13816347+rajendrakumaryadav@users.noreply.github.com>
2021-11-17 10:25:19 +0530

    Update __init__.py
    documentation update

--- greetworld/__init__.py
+++ greetworld/__init__.py
@@ -2,7 +2,7 @@
 Simple hello world python modules.
 '''
 __name__ = "greetworld"
-__version__ = 0.3
+__version__ = 0.3.1
 __author__ = 'Rajendra Kumar R Yadav'
 __doc__ = '''
 A simple Greet World module in Python
